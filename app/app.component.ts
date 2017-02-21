3aa084775fd013d1ec1db6bdb7a3e5d5c830bbeb
AdminMGorka <Mihalchik@pukhovitchi.minsk.belpost.by>
2017-02-21 10:29:35 +0300

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'purchase-app',
+    template: `
+    <div>
+      <p>Карта Yandex</p>
+    </div`
+}
+)
+export class AppComponent {}
